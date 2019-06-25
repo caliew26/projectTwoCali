14b31f09d93b38a61cafa99ddf9df0ea8253e2e6
Bobby Quach <BobbyQuach90@gmail.com>
2019-06-25 17:46:16 -0400

    comment box

--- frontEndStuff/js/init.js
+++ frontEndStuff/js/init.js
@@ -5,3 +5,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
