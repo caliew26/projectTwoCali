161f9569e8aac4abde7e7020399adf5b3fcc161f
kale <intharaksak@gmail.com>
2019-06-29 10:02:54 -0400

    add jquery to gamersMingle.js

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.set("view engine", "hbs")
 app.set("views", path.join(__dirname, "app/views"))
 console.log(__dirname)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 
 
@@ -45,9 +44,7 @@ console.log(__dirname)
 // app.set('view engine', '.hbs')
 
 
-=======
->>>>>>> 67196e5a3a187bb0d37ac2b5da320f53057cdf2d
-=======
+
 var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
@@ -57,7 +54,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 
->>>>>>> 7b147d7eca4562af9d1558414fc175d038fca269
+
 
 //Models
 var models = require("./app/models")
